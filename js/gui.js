378c49fa635f0f3f675b7ef96608820cd64f0c95
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2013-12-20 16:42:00 +0200

    Add fullscreen button.

--- js/gui.js
+++ js/gui.js
@@ -23,6 +23,9 @@ GUI.prototype.initGUI = function() {
 
 	var gui = self.gui = new dat.GUI();
 
+	gui.add(self, 'fullscreen')
+		.name('Toggle Fullscreen');
+
 	gui.add(self, 'channel', self.channels)
 		.name('Channel')
 		.onFinishChange(self.loadGraph.bind(self));
@@ -38,6 +41,27 @@ GUI.prototype.loadGraph = function() {
 	d3.json(self.api + self.channel + '.json', function (data) {
 		self.graph = initGraph(data);
 	});
+};
+
+GUI.prototype.fullscreen = function() {
+	if (!document.fullscreenElement &&    // alternative standard method
+		!document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+		if (document.documentElement.requestFullscreen) {
+			document.documentElement.requestFullscreen();
+		} else if (document.documentElement.mozRequestFullScreen) {
+			document.documentElement.mozRequestFullScreen();
+		} else if (document.documentElement.webkitRequestFullscreen) {
+			document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+		}
+	} else {
+		if (document.cancelFullScreen) {
+			document.cancelFullScreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitCancelFullScreen) {
+			document.webkitCancelFullScreen();
+		}
+	}
 };
 
 GUI.prototype.init = function() {
